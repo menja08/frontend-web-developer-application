5ff2019818b0a1d2078a0331f2ac3346dc4f7a55
ivan <menja08@yahoo.com>
2019-01-17 12:47:49 +0100

    working/functioning app

--- README.md
+++ README.md
@@ -1,23 +1,20 @@
 # Frontend Web Developer Application
 
-We're happy you're interested in applying for the position of `Frontend Web developer` at **SportCompass**.
+The html file is myApp.html
 
-We'd like you to create an application to showcase some of your abilities. You can make this as simple or as complex as you'd like. You can use the folder structure in this repository as a way of organizing your project if you want but it's not required.
+I used Sass for styling.
 
-Here's what you need to do;
-
-1. Design a simple product listing page for an e-commerce store.
-2. Have a responsive layout. (prefferably use Bootstrap4)
-3. Each product should have at least one image.
-4. Give the user the ability to add/edit/delete items in their cart. (use your JS skills here).
-5. Add a readme to your project with a short description of your solution. (can be as short as 2 sentences)
-6. When you're happy with your solution don't forget to send it to us! (repository link or zip)
-
-Bonus points; (in case you want to have more fun)
-
-1. Swap broken images on error with a default one.
-2. Persist the data.
-3. Add some testing.
-
-
-Good luck!
+There are 3 products(3 smartphones). The customer
+has a chance to see how many products that he/she
+has clicked on(this is the value displayed
+beside the cart), and at the same time see the total
+cost/price of all products that have been clicked on.
+ 
+ The "add to cart" button adds 1 element to the
+ cart on every click.
+ 
+ The customer can delete all items in the cart by
+ clicking the "delete All" button.
+ 
+ The customer can remove a particular element from
+ the cart clicking on the "remove item" button
--- assets/js/default.js
+++ assets/js/default.js
@@ -1,198 +1,252 @@
 
-// for mobile
+
 function runImageSlider() {
 
     // get image slider
     let itemContainer = document.getElementById("itemContainer");
-    
-    let sliderLength = itemContainer.children.length;
+    var sliderLength = itemContainer.children.length;
     let itemContainerChildren = itemContainer.children;
-
-    let screenSize = window.innerWidth;
-    if (screenSize < 401) {
-    itemContainer.style.backgroundColor = "lightblue";
-    for(let i=1; i < sliderLength; i++) {
-	itemContainerChildren[i].style.display = "none";
-    }
-    
-    // first image shown on loading
-    let currentImage = itemContainerChildren[0];
-
-    // the click function
-    function leftArrowClicked() {
-	leftArrow = document.getElementById("leftArrow");
-
-	leftArrow.addEventListener("click", function() {
-
-	    leftArrow.style.backgroundColor = "red";
-	    console.log("clicked");
-
-	    //first image
-
-	    if(currentImage === itemContainerChildren[0]) {
-
-		//currentImage.style.display = "none";
-		currentImage = itemContainerChildren[sliderLength-1];
-		currentImage.style.display = "flex";
-
-		// loop over the rest of elements and hide them
+	// for mobile
+    let screenSize = window.outerWidth;
+	if (screenSize < 401) {
 		for(let i=0; i < sliderLength; i++) {
-		    if(itemContainerChildren[i] !== currentImage) {
-			itemContainerChildren[i].style.display = "none";
-			console.log("one");
-		    }
+			 itemContainerChildren[i].style.display = "none";
+			 itemContainerChildren[0].style.display = "flex";
 		}
 
-		// clicked image was not first image
-		// display previous image
-	    } else {
-		currentImage = itemContainerChildren[i-1];
-		currentImage.style.display = "flex";
+		// first image shown on loading
+		var currentImage = 0;
 
-		// loop over the rest of the elements and hide them
-		for(let i=0; i < sliderLength; i++) {
-		    if(itemContainerChildren[i] !== currentImage) {
-			itemContainerChildren[i].style.display = "none";
-			console.log("one");
-		    }
+		// the click function
+		function leftArrowClicked() {
+			let leftArrow = document.getElementById("leftArrow");
+			leftArrow.addEventListener("click", function() {
+				//first image
+				if(currentImage <= sliderLength) {
+				    let prev = currentImage - 1;
+				    currentImage--;
+				    if(prev < 0) {
+				        prev = sliderLength - 1;
+				        currentImage = sliderLength - 1;
+                    }
+					itemContainerChildren[prev].style.display = "flex";
+
+					// loop over the rest of elements and hide them
+					for(let i=0; i < sliderLength; i++) {
+						if(i !== prev) {
+							itemContainerChildren[i].style.display = "none";
+						}
+					}
+				}
+
+			});
+
 		}
-	    }
 
-	    // is this line necessary
-	    return currentImage;
-	});
+		leftArrowClicked();
 
-    }
+		//rightArrow
+		function rightArrowClicked() {
+			let rightArrow = document.getElementById("rightArrow");
 
-    leftArrowClicked();
+			rightArrow.addEventListener("click", function() {
 
-    //rightArrow
-    function rightArrowClicked() {
-	rightArrow = document.getElementById("rightArrow");
+				//first image
+				if(currentImage <= sliderLength) {
+					let next = currentImage + 1;
+					currentImage++;
+					if(next === sliderLength) {
+						next = 0;
+						currentImage = 0;
+					}
+					itemContainerChildren[next].style.display = "flex";
 
-	rightArrow.addEventListener("click", function() {
+					// loop over elements and hide them
+					for(let i=0; i < sliderLength; i++) {
+						if(i !== next) {
+							itemContainerChildren[i].style.display = "none";
+						}
+					}
+				}
 
-	    rightArrow.style.backgroundColor = "red";
-	    console.log("clicked");
-
-	    //first image
-
-	    if(currentImage === itemContainerChildren[0]) {
-
-		//currentImage.style.display = "none";
-		currentImage = itemContainerChildren[sliderLength-1];
-		currentImage.style.display = "flex";
-
-		// loop over elements and hide them
-		for(let i=0; i < sliderLength; i++) {
-		    if(itemContainerChildren[i] !== currentImage) {
-			itemContainerChildren[i].style.display = "none";
-			console.log("one");
-		    }
+			});
 		}
-		
-	    }
-
-	    // is this line necessary
-	    return currentImage;
-	});
-    }
-	rightArrowClicked();
-    } // end of screenSize if statement
+		rightArrowClicked();
+	} // end of screenSize if statement
 
     // select the counter
     counter = document.getElementById("counter");
-    let itemCounter = 0;
+    var itemCounter = 0;
+
+    // all three items
+	/*var allItems = {};*/
+
+	// the total
+	let total = document.getElementById("total");
+
+	var totalPrice = 0;
+	/*var totalHTC = 0;
+	var totalHuawei = 0;
+	var totalSamsung = 0;*/
+
+	// cartValues
+	let cartValues = document.getElementById("cartValues");
+	let htc = document.getElementById("htc");
+	let huawei = document.getElementById("huawei");
+	let samsung = document.getElementById("samsung");
 
     // add button1 to cart
-    let button1Counter = 0;
+	var buttonItem1 = {value: 0};
+    var button1Counter = 0;
     let button1 = document.getElementById("button1");
     button1.addEventListener("click", addedButton1);
 
-    function addedButton1() {
-	button1.style.backgroundColor = "green";
-	button1Counter++;
-	itemCounter++;
-	counter.innerHTML = itemCounter;
-    }
+	function addedButton1() {
+		// button1.style.backgroundColor = "green";
+		button1Counter++;
+		// add value of button1Counter to buttonItem1
+		buttonItem1.value = button1Counter;
+		itemCounter++;
+		// allItems[buttonItem1] = buttonItem1.value;
+		counter.innerHTML = itemCounter;
+		htc.innerHTML = buttonItem1.value;
+		// totalHTC = allItems[buttonItem1] * 2500;
+
+	}
 
     // remove button1 from cart
     let removeButton1 = document.getElementById("removeButton1");
     removeButton1.addEventListener("click", removesButton1);
 
-    function removesButton1 () {
-	itemCounter--;
-	button1Counter--;
-	
-	if(button1Counter === 0) {
-	    counter.innerHTML = itemCounter;
+	function removesButton1 () {
+        if(button1Counter > 0){
+            itemCounter--;
+            button1Counter--;
+        }
+        if(itemCounter < 0) {
+            itemCounter = 0;
+        }
+        if(button1Counter < 0) {
+            button1Counter = 0;
+        }
+        buttonItem1.value = button1Counter;
+        htc.innerHTML = buttonItem1.value;
+        // allItems[buttonItem1] = buttonItem1.value;
+        counter.innerHTML = itemCounter;
 	}
-	if(itemCounter == 0) {
-	    counter.innerHTML = itemCounter;
+
+	// item2
+	// add button2 to cart
+	var buttonItem2 = {value: 0};
+	var button2Counter = 0;
+	let button2 = document.getElementById("button2");
+	button2.addEventListener("click", addedButton2);
+
+	function addedButton2() {
+		// button2.style.backgroundColor = "green";
+		button2Counter++;
+		itemCounter++;
+		buttonItem2.value = button2Counter;
+		counter.innerHTML = itemCounter;
+		huawei.innerHTML = buttonItem2.value;
+		// allItems[buttonItem2] = buttonItem2.value;
+		// totalHuawei = allItems[buttonItem2] * 3500;
 	}
-	counter.innerHTML = itemCounter;
-    }
 
-    // item2
-        // add button2 to cart
-    let button2Counter = 0;
-    let button2 = document.getElementById("button2");
-    button2.addEventListener("click", addedButton2);
+	// remove button2 from cart
+	let removeButton2 = document.getElementById("removeButton2");
+	removeButton2.addEventListener("click", removesButton2);
 
-    function addedButton2() {
-	button2.style.backgroundColor = "green";
-	button2Counter++;
-	itemCounter++;
-	counter.innerHTML = itemCounter;
-    }
+	function removesButton2 () {
+		if(button2Counter > 0){
+            itemCounter--;
+            button2Counter--;
+        }
 
-    // remove button2 from cart
-    let removeButton2 = document.getElementById("removeButton2");
-    removeButton2.addEventListener("click", removesButton2);
-
-    function removesButton2 () {
-	itemCounter--;
-	button2Counter--;
-	
-	if(button2Counter === 0) {
-	    counter.innerHTML = itemCounter;
+        if(itemCounter < 0) {
+            itemCounter = 0;
+        }
+        if(button2Counter < 0) {
+            button2Counter = 0;
+        }
+		counter.innerHTML = itemCounter;
+		buttonItem2.value = button2Counter;
+		huawei.innerHTML = buttonItem2.value;
+		// allItems[buttonItem2] = buttonItem2.value;
 	}
-	if(itemCounter == 0) {
-	    counter.innerHTML = itemCounter;
+
+
+	// item3
+	// add button3 to cart
+	var buttonItem3 = {value: 0};
+	var button3Counter = 0;
+	let button3 = document.getElementById("button3");
+	button3.addEventListener("click", addedButton3);
+
+	function addedButton3() {
+		button3Counter++;
+		itemCounter++;
+		buttonItem3.value = button3Counter;
+		counter.innerHTML = itemCounter;
+		samsung.innerHTML = buttonItem3.value;
+		// allItems[buttonItem3] = buttonItem3.value;
+		// totalSamsung = allItems[buttonItem3] * 4500;
 	}
-	counter.innerHTML = itemCounter;
-    }
 
+	// remove button3 from cart
+	let removeButton3 = document.getElementById("removeButton3");
+	removeButton3.addEventListener("click", removesButton3);
 
-    // item3
-        // add button3 to cart
-    let button3Counter = 0;
-    let button3 = document.getElementById("button3");
-    button3.addEventListener("click", addedButton3);
+	function removesButton3 () {
+        if(button3Counter > 0){
+            itemCounter--;
+            button3Counter--;
+        }
 
-    function addedButton3() {
-	button3.style.backgroundColor = "green";
-	button3Counter++;
-	itemCounter++;
-	counter.innerHTML = itemCounter;
-    }
-
-    // remove button3 from cart
-    let removeButton3 = document.getElementById("removeButton3");
-    removeButton3.addEventListener("click", removesButton3);
-
-    function removesButton3 () {
-	itemCounter--;
-	button3Counter--;
-	
-	if(button3Counter === 0) {
-	    counter.innerHTML = itemCounter;
+        if(itemCounter < 0) {
+            itemCounter = 0;
+        }
+        if(button3Counter < 0) {
+            button3Counter = 0;
+        }
+		counter.innerHTML = itemCounter;
+		buttonItem3.value = button3Counter;
+		samsung.innerHTML = buttonItem3.value;
+		// allItems[buttonItem3] = buttonItem3.value;
 	}
-	if(itemCounter == 0) {
-	    counter.innerHTML = itemCounter;
+
+	total.addEventListener("click", totalNumberOfItems);
+	function totalNumberOfItems() {
+		// let myArray = Object.keys(allItems);
+		let numberOfButton1 = buttonItem1.value;
+		let numberOfButton2 = buttonItem2.value;
+		let numberOfButton3 = buttonItem3.value;
+		let priceOfButton1 = numberOfButton1 * 2500;
+		let priceOfButton2 = numberOfButton2 * 3500;
+		let priceOfButton3 = numberOfButton3 * 4500;
+		totalPrice = priceOfButton1 + priceOfButton2 + priceOfButton3;
+		total.innerHTML = totalPrice;
 	}
-	counter.innerHTML = itemCounter;
-    }
+
+
+	// deleteAll
+	let deleteAll = document.getElementById("deleteAll");
+	deleteAll.addEventListener("click", deleteAllItems);
+
+	function deleteAllItems() {
+		itemCounter = 0;
+		totalPrice = 0;
+		button1Counter = 0;
+		button2Counter = 0;
+		button3Counter = 0;
+		htc.innerHTML = button1Counter;
+		huawei.innerHTML = button2Counter;
+		samsung.innerHTML = button3Counter;
+        total.innerHTML = totalPrice;
+        counter.innerHTML = itemCounter;
+
+	}
+
 }
 
 runImageSlider();
